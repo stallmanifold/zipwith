ee6523ff486b346e006930a651d3a452eb1baed7
LambdaXymox <lambda.xymox@gmail.com>
2016-05-10 03:58:33 -0500

    Add unit tests.

--- src/tests.rs
+++ src/tests.rs
@@ -5,7 +5,7 @@ use std::ops::Fn;
 
 #[test]
 fn test_zip_with() {
-    let add = |x: usize, y: usize| { x+y as usize };
+    let add = |x: usize, y: usize| { x + y as usize };
     let vec1: Vec<usize> = vec![0x01; 16];
     let vec2: Vec<usize> = vec![0x02; 16];
     let zipper   = ZipWith::<&Fn(usize,usize)->usize,Vec<usize>,Vec<usize>,Vec<usize>>::zip_with(&add, vec1, vec2);
@@ -13,4 +13,52 @@ fn test_zip_with() {
     let expected = vec![0x03; 16];
 
     assert_eq!(expected, result); 
+}
+
+#[test]
+fn test_zip_with_output_length_should_be_identical_to_shortest_vector() {
+    let add = |x: usize, y: usize| { x + y as usize };
+    let vec1: Vec<usize> = vec![0x01; 16];
+    let vec2: Vec<usize> = vec![0x02; 20];
+    let zipper   = ZipWith::<&Fn(usize,usize)->usize,Vec<usize>,Vec<usize>,Vec<usize>>::zip_with(&add, vec1, vec2);
+    let result   = zipper.collect::<Vec<usize>>();
+    let expected = vec![0x03; 16];
+
+    assert_eq!(expected.len(), result.len());
+}
+
+#[test]
+fn test_zip_with_having_one_empty_input() {
+    let add              = |x: usize, y: usize| { x + y as usize };
+    let vec1: Vec<usize> = vec![];
+    let vec2: Vec<usize> = vec![0x02; 16];
+    let zipper           = ZipWith::<&Fn(usize,usize)->usize,Vec<usize>,Vec<usize>,Vec<usize>>::zip_with(&add, vec1, vec2);
+    let result           = zipper.collect::<Vec<usize>>();
+    let expected: Vec<usize> = vec![];
+
+    assert_eq!(expected.len(), result.len());
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn test_zip_with_having_two_empty_inputs() {
+    let add              = |x: usize, y: usize| { x + y as usize };
+    let vec1: Vec<usize> = vec![];
+    let vec2: Vec<usize> = vec![];
+    let zipper           = ZipWith::<&Fn(usize,usize)->usize,Vec<usize>,Vec<usize>,Vec<usize>>::zip_with(&add, vec1, vec2);
+    let result           = zipper.collect::<Vec<usize>>();
+
+    assert_eq!(result.len(), 0);
+}
+
+#[test]
+fn test_zip_with_iterator() {
+    let xor              = |x: usize, y: usize| { x ^ y as usize };
+    let vec1: Vec<usize> = vec![0xFF; 16];
+    let vec2: Vec<usize> = vec![0xF0; 16];
+    let zipper           = ZipWith::<&Fn(usize,usize)->usize,Vec<usize>,Vec<usize>,Vec<usize>>::zip_with(&xor, vec1, vec2);
+
+    for word in zipper {
+        assert_eq!(word, 0x0F);
+    } 
 }
