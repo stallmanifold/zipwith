7ffba886d1c2c86bfebc004e06e140a0ea0725c3
LambdaXymox <lambda.xymox@gmail.com>
2016-05-10 13:07:52 -0500

    Add unit test.

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,7 @@
 use zipwith::ZipWith;
 use std::iter::Iterator;
 use std::ops::Fn;
+use std::collections::linked_list::LinkedList;
 
 
 #[test]
@@ -73,4 +74,24 @@ fn test_zip_with_sized_iterator() {
     let zipper           = ZipWith::<&Fn(&usize,&usize)->usize,&[usize],&[usize],usize>::zip_with(&xor, slice1, slice2);
     
     assert_eq!(zipper.len(), vec1.len());
+}
+
+#[test]
+fn test_zip_with_should_work_with_mixed_types() {
+    let xor                         = |x: &usize, y: &usize| { *x ^ *y as usize };
+    let vec: Vec<usize>             = vec![0xFF; 16];
+    let mut list: LinkedList<usize> = LinkedList::new();
+
+    for item in &vec { // Borrow vec instead of moving it.
+        list.push_back(item.clone());
+    }
+
+    let slice: &[usize]      = vec.as_slice();
+    let zipper = ZipWith::<&Fn(&usize,&usize)->usize,&[usize],&LinkedList<usize>,usize>::zip_with(&xor, slice, &list);
+    let result: Vec<usize>   = zipper.collect::<Vec<usize>>();
+    let expected: Vec<usize> = vec![0x00; 16];
+
+    assert_eq!(result.len(), expected.len());
+    assert_eq!(result, expected);
+
 }
